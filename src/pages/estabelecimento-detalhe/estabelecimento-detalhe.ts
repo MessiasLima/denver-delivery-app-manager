23d21667cd8f11102e5f0f358c6b25b8a1c8ee60
messiaslima <messiaslima.03@gmail.com>
2018-05-01 16:10:09 -0300

    salvar formas de pagamento por estabelecimento

--- src/pages/estabelecimento-detalhe/estabelecimento-detalhe.ts
+++ src/pages/estabelecimento-detalhe/estabelecimento-detalhe.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController, Events } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, Events, AlertController, ToastController } from 'ionic-angular';
 import { Estabelecimento } from '../../model/estabelecimento';
 import { EstabelecimentoDetalheInfoComponent } from './estabelecimento-detalhe-info/estabelecimento-detalhe-info';
 import { EstabelecimentoNewComponent } from '../estabelecimentos/estabelecimento-new/estabelecimento-new';
 import { UsuariosAdmPage } from '../usuarios-adm/usuarios-adm';
 import { CardapioPage } from '../cardapio/cardapio';
 import { DownloadProvider } from '../../providers/download/download';
+import { FormaPagamentoProvider } from '../../providers/forma-pagamento/forma-pagamento';
+import { FormaPagamento } from '../../model/forma-pagamento';
 
 @IonicPage()
 @Component({
@@ -16,16 +18,23 @@ export class EstabelecimentoDetalhePage {
 
 	estabelecimento: Estabelecimento;
 	estabelecimentoOriginal: Estabelecimento;
+	formasPagamento: FormaPagamento[];
+	formasPagamentoSelecionadas: FormaPagamento[];
 
 	constructor(
 		public navCtrl: NavController,
 		public navParams: NavParams,
 		public modalControler: ModalController,
 		private events: Events,
-		private downloadProvider: DownloadProvider
+		private downloadProvider: DownloadProvider,
+		private formaPagamentoProvider: FormaPagamentoProvider,
+		private alertController: AlertController,
+		private toastController: ToastController
 	) {
 		this.estabelecimento = navParams.get("data");
 		this.estabelecimentoOriginal = navParams.get("data");
+		this.listarFormasPagamento();
+		this.listarFormasPagamentoSelecionadas();
 	}
 
 	ionViewDidLoad() {
@@ -60,5 +69,81 @@ export class EstabelecimentoDetalhePage {
 
 	openProducts() {
 		this.navCtrl.push(CardapioPage, { estabelecimento: this.estabelecimento });
+	}
+
+	private listarFormasPagamento() {
+		this.formaPagamentoProvider.listarFormasDePagamento().subscribe(
+			(data: any) => {
+				this.formasPagamento = data;
+			}
+		);
+	}
+
+	private listarFormasPagamentoSelecionadas() {
+		this.formaPagamentoProvider.listarFormasPagamentoSelecionadas(this.estabelecimento).subscribe(
+			(data: any) => {
+				this.formasPagamentoSelecionadas = data;
+			}
+		);
+	}
+
+	promptFormasPagamento() {
+		let alert = this.alertController.create();
+		alert.setTitle("Formas de pagamento");
+		alert.setMessage("Selecione as formas de pagamento que seu estabelecimento aceita");
+
+		this.formasPagamento.forEach(formaPagamento => {
+			alert.addInput({
+				type: 'checkbox',
+				label: formaPagamento.nome,
+				value: JSON.stringify(formaPagamento),
+				checked: this.verificarSeFormaPagamentoFoiSelecionada(formaPagamento)
+			});
+		});
+
+		alert.addButton('Cancel');
+
+		alert.addButton({
+			text: 'Okay',
+			handler: data => {
+				this.salvarFormasDePagamentoSelecionadas(data);
+			}
+		});
+
+		alert.present();
+	}
+
+	verificarSeFormaPagamentoFoiSelecionada(formaPagamento: FormaPagamento): boolean {
+		let selecionada = false;
+		this.formasPagamentoSelecionadas.forEach(element => {
+			if (element.id == formaPagamento.id) {
+				selecionada = true;
+			}
+		});
+		return selecionada;
+	}
+
+	private salvarFormasDePagamentoSelecionadas(data: string[]) {
+		let formaPagamentoParaSalvar: FormaPagamento[] = [];
+		data.forEach(element => {
+			formaPagamentoParaSalvar.push(JSON.parse(element));
+		});
+		this.formaPagamentoProvider.salvarFormaPagamentoPorEstabelecimento(formaPagamentoParaSalvar, this.estabelecimento).subscribe(
+			()=>{
+				this.mostrarMensagem("Formas de pagamento salvas");
+				this.listarFormasPagamento();
+				this.listarFormasPagamentoSelecionadas();
+			},
+			(err)=>{
+				this.mostrarMensagem("Falha ao salvar formas de pagamento");
+			}
+		);
+	}
+
+	private mostrarMensagem(mensagem: string) {
+		this.toastController.create({
+			message: mensagem,
+			duration: 3000
+		}).present();
 	}
 }
--- src/providers/forma-pagamento/forma-pagamento.ts
+++ src/providers/forma-pagamento/forma-pagamento.ts
@@ -2,21 +2,36 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CommonsProvider } from '../commons/commons';
 import { FormaPagamento } from '../../model/forma-pagamento';
+import { Estabelecimento } from '../../model/estabelecimento';
 
 @Injectable()
 export class FormaPagamentoProvider {
 
 	private ENDPOINT_FORMA_PAGAMENTO: string;
+	private ENDPOINT_FORMA_PAGAMENTO_ESTABELECIMENTO: string;
 
 	constructor(private http: HttpClient, private commonsProvider: CommonsProvider) {
-		this.ENDPOINT_FORMA_PAGAMENTO = commonsProvider.getHost() + "/forma-pagamento"
+		this.ENDPOINT_FORMA_PAGAMENTO = commonsProvider.getHost() + "/forma-pagamento";
+		this.ENDPOINT_FORMA_PAGAMENTO_ESTABELECIMENTO = this.ENDPOINT_FORMA_PAGAMENTO + "/estabelecimento/";
 	}
 
 	listarFormasDePagamento() {
 		return this.http.get(this.ENDPOINT_FORMA_PAGAMENTO, this.commonsProvider.getCommonHeaders());
 	}
 
+	listarFormasPagamentoSelecionadas(estabelecimento: Estabelecimento) {
+		return this.http.get(this.ENDPOINT_FORMA_PAGAMENTO_ESTABELECIMENTO + estabelecimento.id, this.commonsProvider.getCommonHeaders());
+	}
+
 	salvar(formaPagamento: FormaPagamento) {
 		return this.http.post(this.ENDPOINT_FORMA_PAGAMENTO, formaPagamento, this.commonsProvider.getPayloadHeaders());
+	}
+
+	salvarFormaPagamentoPorEstabelecimento(formaPagamentos: FormaPagamento[], estabelecimento:Estabelecimento){
+		return this.http.post(
+			this.ENDPOINT_FORMA_PAGAMENTO_ESTABELECIMENTO + estabelecimento.id,
+			formaPagamentos,
+			this.commonsProvider.getPayloadHeaders()
+		);
 	}
 }
